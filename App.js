a5de50e7baf2dfdfcf7cbeab3a8e70aba0faa0e0
kemboi590 <bkemboi590@gmail.com>
2023-05-12 21:00:37 +0300

    added the submit functionality

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const myList = document.querySelector('tbody');
-  
+    const addForm = document.getElementById('addForm');
+
     myList.addEventListener('click', (e) => {
-      if (e.target.classList.contains('delete')) {
-        const tr = e.target.closest('tr');
-        tr.remove();
-      }
+        if (e.target.classList.contains('delete')) {
+            const tr = e.target.closest('tr');
+            tr.remove();
+        }
     });
-  });
-  
+
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+    
+});
